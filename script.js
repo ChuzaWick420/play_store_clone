b048de4a8a722b4b6b1343cef8abc71b0fc01619
glutonium69 <tahzib404@gmail.com>
2023-10-14 21:59:09 +0600

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+// let divs = document.querySelectorAll('div');
+
+// divs.forEach(div => {
+//     let r = Math.floor(Math.random() * 255);
+//     let g = Math.floor(Math.random() * 255);
+//     let b = Math.floor(Math.random() * 255);
+
+//     div.style.background = `rgb(${r}, ${g}, ${b})`
+// })
